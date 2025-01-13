59c6e9a5309c5ca423cc0c002c6513221f9cdb55
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-13 16:59:29 +0900

    ok

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Title from './Title'
+import { services } from '../data'
 
 const Service = () => {
   return (
@@ -8,38 +9,21 @@ const Service = () => {
       <Title title='our' subTitle='services' />
       
       <div className="section-center services-center">
-        <article className="service">
-          <span className="service-icon"><i className="fas fa-wallet fa-fw"></i></span>
-          <div className="service-info">
-            <h4 className="service-title">saving money</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
 
-        <article className="service">
-          <span className="service-icon"><i className="fas fa-tree fa-fw"></i></span>
+      {services.map((s) => {
+        return (
+          <article className="service">
+          <span className="service-icon"><i className={s.icon}></i></span>
           <div className="service-info">
-            <h4 className="service-title">endless hiking</h4>
+            <h4 className="service-title">{s.title}</h4>
             <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
+              {s.text}
             </p>
           </div>
         </article>
+        )
+      })}
 
-        <article className="service">
-          <span className="service-icon"><i className="fas fa-socks fa-fw"></i></span>
-          <div className="service-info">
-            <h4 className="service-title">amazing comfort</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
       </div>
     </section>
   )
